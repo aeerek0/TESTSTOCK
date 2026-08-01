8ccc749cc6605102f1da108e037c300316b84c82
aeerek0 <aeerek0@gmail.com>
2026-08-01 09:42:05 +0700

    Update goals.js

--- goals.js
+++ goals.js
@@ -9,100 +9,21 @@ function saveGoals() {
     const goals = {
 
         portfolioGoal: Number(document.getElementById("inputPortfolioGoal").value) || 0,
-
         dividendGoal: Number(document.getElementById("inputDividendGoal").value) || 0,
-
         monthlyInvestment: Number(document.getElementById("inputMonthlyInvestment").value) || 0,
-
         expectedReturn: Number(document.getElementById("inputExpectedReturn").value) || 8,
-
         targetYear: document.getElementById("inputTargetYear").value
 
     };
-
 
     localStorage.setItem(
         "investmentGoals",
         JSON.stringify(goals)
     );
 
-
     alert("✅ บันทึกเป้าหมายเรียบร้อย");
 
-
     loadGoals();
-
-    // =========================
-// Current Portfolio
-// =========================
-const currentPortfolio =
-    Number(localStorage.getItem("currentPortfolioValue")) || 0;
-
-const currentDividend =
-    Number(localStorage.getItem("currentDividendValue")) || 0;
-
-
-// แสดงข้อมูลปัจจุบัน
-document.getElementById("currentPortfolioValue").innerText =
-    formatNumber(currentPortfolio) + " บาท";
-
-document.getElementById("currentDividendValue").innerText =
-    formatNumber(currentDividend) + " บาท";
-
-
-// แสดงเป้าหมาย
-document.getElementById("targetPortfolioValue").innerText =
-    formatNumber(goals.portfolioGoal) + " บาท";
-
-document.getElementById("targetDividendValue").innerText =
-    formatNumber(goals.dividendGoal) + " บาท";
-
-
-// =========================
-// Portfolio Progress
-// =========================
-let portfolioPercent = 0;
-
-if (goals.portfolioGoal > 0) {
-
-    portfolioPercent =
-        (currentPortfolio / goals.portfolioGoal) * 100;
-
-}
-
-portfolioPercent = Math.min(portfolioPercent, 100);
-
-document.getElementById("goalProgressPercent").innerText =
-    portfolioPercent.toFixed(2);
-
-document.getElementById("goalProgressBar").style.width =
-    portfolioPercent + "%";
-
-document.getElementById("remainingGoal").innerText =
-    formatNumber(
-        Math.max(goals.portfolioGoal - currentPortfolio, 0)
-    );
-
-
-// =========================
-// Dividend Progress
-// =========================
-let dividendPercent = 0;
-
-if (goals.dividendGoal > 0) {
-
-    dividendPercent =
-        (currentDividend / goals.dividendGoal) * 100;
-
-}
-
-dividendPercent = Math.min(dividendPercent, 100);
-
-document.getElementById("dividendGoalPercent").innerText =
-    dividendPercent.toFixed(2);
-
-document.getElementById("dividendGoalProgressBar").style.width =
-    dividendPercent + "%";
 
 }
 
